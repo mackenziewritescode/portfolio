b755d7f1fcab91e2d56dd2ebd5da70199d93ab7e
mackenziewritescode <mackenzie.charlton@gmail.com>
2020-10-08 15:14:56 -0400

    adding base style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.scss";
+import { ImArrowDown } from "react-icons/im";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+
+const arrows = (
+  <div className="arrows">
+    <ImArrowDown className="arrow" /> <ImArrowDown className="arrow" />{" "}
+    <ImArrowDown className="arrow" />
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
+      {/* --------------------------------------------------  PAGE 1  */}
       <div id="page-1" className="page">
         <p className="main-text" id="page-1-block-1">
           Hello.
@@ -17,7 +28,9 @@ function App() {
         <p className="bottom-text" id="page-1-block-4">
           Scroll down to learn more
         </p>
+        {arrows}
       </div>
+      {/* ------------------------------------------------  PAGE 2  */}
       <div id="page-2" className="page">
         <p className="main-text" id="page-2-block-1">
           I specialize in front-end web development.
@@ -31,39 +44,188 @@ function App() {
         <p className="bottom-text" id="page-2-block-4">
           check out some of my recent projects
         </p>
+        {arrows}
       </div>
-      <div id="page-1" className="page">
-        <div className="project">
-          <a className="project-img-link" href="" alt="">
-            <img></img>
-          </a>
-          <h3 className="project-title">Project Title</h3>
-          <p className="-project-description">
-            This is a description about the project.
-          </p>
-          <a className="project-link" href="" alt="">
-            Visit
-          </a>
-          <a className="project-github" href="" alt="">
-            Github
-          </a>
+      {/* ------------------------------------------------  PAGE 3  */}
+      <div id="page-3" className="page">
+        <h2 id="page-3-title">Projects</h2>
+        <div id="project-wrap">
+          <div className="project">
+            <a
+              className="project-img-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              <img alt=""></img>
+            </a>
+            <h3 className="project-title">Project Title</h3>
+            <p className="project-description">
+              This is a description about the project.
+            </p>
+            <a
+              className="project-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              Visit
+            </a>
+            <a
+              className="project-github"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              GitHub
+            </a>
+          </div>
+          <div className="project">
+            <a
+              className="project-img-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              <img alt=""></img>
+            </a>
+            <h3 className="project-title">Project Title</h3>
+            <p className="project-description">
+              This is a description about the project.
+            </p>
+            <a
+              className="project-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              Visit
+            </a>
+            <a
+              className="project-github"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              GitHub
+            </a>
+          </div>
+          <div className="project">
+            <a
+              className="project-img-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              <img alt=""></img>
+            </a>
+            <h3 className="project-title">Project Title</h3>
+            <p className="project-description">
+              This is a description about the project.
+            </p>
+            <a
+              className="project-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              Visit
+            </a>
+            <a
+              className="project-github"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              GitHub
+            </a>
+          </div>
+          <div className="project">
+            <a
+              className="project-img-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              <img alt=""></img>
+            </a>
+            <h3 className="project-title">Project Title</h3>
+            <p className="project-description">
+              This is a description about the project.
+            </p>
+            <a
+              className="project-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              Visit
+            </a>
+            <a
+              className="project-github"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              GitHub
+            </a>
+          </div>
+          <div className="project">
+            <a
+              className="project-img-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              <img alt=""></img>
+            </a>
+            <h3 className="project-title">Project Title</h3>
+            <p className="project-description">
+              This is a description about the project.
+            </p>
+            <a
+              className="project-link"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              Visit
+            </a>
+            <a
+              className="project-github"
+              href="http://www.sunkenworld.com/"
+              alt=""
+            >
+              GitHub
+            </a>
+          </div>
         </div>
-        <div className="project">
-          <a className="project-img-link" href="" alt="">
-            <img></img>
+        {arrows}
+      </div>
+      {/* ------------------------------------------------  PAGE 4  */}
+      <div id="page-4" className="page">
+        <p className="main-text" id="page-4-block-1">
+          You can see all the code for each project, along with detailed
+          descriptions, on my{" "}
+          <a href="http://www.sunkenworld.com/" alt="">
+            GitHub
           </a>
-          <h3 className="project-title">Project Title</h3>
-          <p className="-project-description">
-            This is a description about the project.
-          </p>
-          <a className="project-link" href="" alt="">
-            Visit
-          </a>
-          <a className="project-github" href="" alt="">
-            Github
-          </a>
+          .
+        </p>
+        <p className="main-text" id="page-4-block-2">
+          Need a web developer? I'd love to help!
+        </p>
+        <p className="main-text" id="page-4-block-3">
+          Get in touch:
+        </p>
+        <div id="contact-wrap">
+          <div className="contact-circle">
+            <MdEmail className="contact-icon" />
+          </div>
+          <div className="contact-circle">
+            <FaLinkedin className="contact-icon" />
+          </div>
         </div>
       </div>
+      <footer>
+        <p>
+          This site was made by{" "}
+          <a className="footer-link" href="http://www.sunkenworld.com/about/">
+            Mackenzie Charlton
+          </a>{" "}
+          in 2020 with{" "}
+          <a className="footer-link" href="https://reactjs.org">
+            React
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 }
